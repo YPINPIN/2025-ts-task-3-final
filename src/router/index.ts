1c7f326be48055dfd7cebdfaa87535bf5e9d7de6
YPINPIN <yping611@gmail.com>
2025-12-18 18:09:58 +0800

    add meta title setting

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,31 +13,37 @@ const router = createRouter({
       path: '/products',
       name: 'products',
       component: () => import('@/views/ProductsView.vue'),
+      meta: { title: '產品列表' },
     },
     {
       path: '/products/:id',
       name: 'product-detail',
       component: () => import('@/views/ProductDetailView.vue'),
+      meta: { title: '產品詳細' },
     },
     {
       path: '/admin',
       name: 'dashboard',
       component: () => import('@/components/Dashboard.vue'),
+      meta: { title: '後台 - 儀表板' },
       children: [
         {
           path: 'product-management',
           name: 'product-management',
           component: () => import('@/views/ProductManagement.vue'),
+          meta: { title: '後台 - 商品管理' },
         },
         {
           path: 'order-management',
           name: 'order-management',
           component: () => import('@/views/OrderManagement.vue'),
+          meta: { title: '後台 - 訂單管理' },
         },
         {
           path: 'coupon-management',
           name: 'coupon-management',
           component: () => import('@/views/CouponManagement.vue'),
+          meta: { title: '後台 - 優惠券管理' },
         },
       ],
     },
@@ -45,6 +51,7 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: () => import('@/views/Login.vue'),
+      meta: { title: '後台 - 登入' },
     },
   ],
   scrollBehavior(to, from, savedPosition) {
@@ -58,6 +65,10 @@ const router = createRouter({
       behavior: 'smooth',
     }
   },
+})
+
+router.afterEach((to) => {
+  document.title = to.meta.title ? '植感生活 | ' + to.meta.title : '植感生活'
 })
 
 export default router
